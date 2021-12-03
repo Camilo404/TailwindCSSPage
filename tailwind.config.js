5844a9e1e7c944ce1bfc67fc6df3ef1e01b16a7e
Camilo404 <78864976+Camilo404@users.noreply.github.com>
2021-12-03 10:53:43 -0500

    Page complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,21 @@ module.exports = {
         './public/**/*.html',
     ],
     darkMode: false,
+    mode: 'jit', 
     theme: {
-        extend: {}
+        extend: {
+            backgroundImage: theme => ({
+                'wave-pattern': 'url(../img/wave-white.png)',
+                'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
+                'pattern-characters-red': 'url(../img/pattern-characters-red.png)',
+                'character-mario': 'url(../img/character-l.png)',
+                'wave-pink': 'url(../img/wave-pink.png)',
+                'yellow-dots': 'url(../img/pattern-yellow-dots.png)',
+            }),
+            screens: {
+                'xsm': '320px'
+            }
+        }
     },
     variants: {
         extend: {},
